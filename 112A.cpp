c2f0952acb8dac1af85f1006ce30fb96bd5797c6
Jasim Ameen <89957373+jasimameen@users.noreply.github.com>
2022-08-24 08:39:57 +0530

    112A

---
+++ 112A.cpp
@@ -0,0 +1,37 @@
+// A. Petya and Strings
+// Little Petya loves presents. His mum bought him two strings of the same size for his birthday. The strings consist of uppercase and lowercase Latin letters. Now Petya wants to compare those two strings lexicographically. The letters' case does not matter, that is an uppercase letter is considered equivalent to the corresponding lowercase letter. Help Petya perform the comparison.
+
+// Input
+// Each of the first two lines contains a bought string. The strings' lengths range from 1 to 100 inclusive. It is guaranteed that the strings are of the same length and also consist of uppercase and lowercase Latin letters.
+
+// Output
+// If the first string is less than the second one, print "-1". If the second string is less than the first one, print "1". If the strings are equal, print "0". Note that the letters' case is not taken into consideration when the strings are compared.
+
+#include<bits/stdc++.h>
+using namespace std;
+void solve();
+int main()
+{
+ios_base::sync_with_stdio(false);cin.tie(NULL);
+
+#ifndef ONLINE_JUDGE
+freopen("input.txt", "r", stdin);
+freopen("error.txt", "w", stderr);
+freopen("output.txt", "w", stdout);
+#endif
+
+int t=1;
+cin>>t;
+while(t--)
+{
+	solve();
+	cout<<"\n";
+}
+
+cerr<<"time taken : "<<(float)clock()/CLOCKS_PER_SEC<<" secs"<<endl;
+return 0;
+}
+void solve()
+{
+	cout << "Hellow World";
+}
