b006ac010fbbc9b2ce26109428b92c858a778fb1
Jasim Ameen <89957373+jasimameen@users.noreply.github.com>
2022-08-24 09:46:20 +0530

    temporary

--- temporary.cpp
+++ temporary.cpp
@@ -6,8 +6,12 @@ int main() {
 
 	string s1 = "a", s2 ="b";
 
-	if (s1>s2) cout << "greator";
-	else cout << "\nlower\n\n";
+	cout << "\n\n";
+
+	if (s1>s2) cout << s1 << " is greator";
+	else cout << s1 << "is lower";
+
+	cout << "\n\n";
 
 	return 0;
 }
