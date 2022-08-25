ab8e7c2e160a19f2e8749e7b75f4ea830d228254
Jasim Ameen <89957373+jasimameen@users.noreply.github.com>
2022-08-26 00:22:35 +0530

    122A

--- 122A.cpp
+++ 122A.cpp
@@ -4,9 +4,12 @@ using namespace std;
 
 string solve(int n)
 {	
-	if (n%4 == 0 || n%7 == 0) return "YES";
-
-
+	
+	if (n%4==0||n%7==0||
+		n%44==0||n%47==0||n%74==0||n%77==0||
+		n%444==0||n%447==0||n%74==0||n%477==0||n%744==0||n%747==0||n%774==0||n%777==0) 
+		return "YES";
+	
 	return "NO";
 }
 
